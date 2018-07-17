088d8a2ab947607430c77082f71f453dbe61812c
hamza <hbenderouach@sqli.com>
2018-07-17 18:07:31 +0200

    Init project

--- vars/gitlabPurne.groovy
+++ vars/gitlabPurne.groovy
@@ -1,6 +1,6 @@
 def call(def urlGit) {
      withCredentials([usernamePassword(credentialsId: 'GIT_TEST')]) {
-		        bat "git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@${url}"	 
+		        //bat "git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@${url}"	 
 		        bat "git fetch --all --prune"
 	 }
 }
