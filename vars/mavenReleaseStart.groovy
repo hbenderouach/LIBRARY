76a57110176d05dedff804b760e939972e7a4961
hamza <hbenderouach@sqli.com>
2018-07-19 10:31:35 +0200

    Init project

--- vars/mavenReleaseStart.groovy
+++ vars/mavenReleaseStart.groovy
@@ -1,5 +1,5 @@
 def call(def releaseVersionParam, def developmentVersionParam) {
      withCredentials([usernamePassword(credentialsId: 'GIT_TEST', passwordVariable: 'AEM_PASSWORD', usernameVariable: 'AEM_USERNAME')]) {
-     bat "mvn clean jgitflow" :release-start -DreleaseVersion=1.0.0 -DdevelopmentVersion=1.0.0-SNAPSHOT"
+     bat "mvn clean jgitflow" :release-start -DreleaseVersion=1.0.0 -DdevelopmentVersion=2.4.1-SNAPSHOT"
     }
 }
