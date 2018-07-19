a27548cf9f61f69654eb51f156d186a3fb4c5e40
hamza <hbenderouach@sqli.com>
2018-07-19 10:22:02 +0200

    Init project

--- vars/mavenPushBranchStarted.groovy
+++ vars/mavenPushBranchStarted.groovy
@@ -1,6 +1,6 @@
 def call(def releaseVersionParam,def urlGit, def liveCycle) {
      withCredentials([usernamePassword(credentialsId: 'GIT_AEM', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-		        bat "git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@${url}"	 
-		        bat "git push --set-upstream origin ${liveCycle}-${releaseVersionParam}"
+		        //bat "git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@${url}"	 
+		        //bat "git push --set-upstream origin ${liveCycle}-${releaseVersionParam}"
 	 }
 }
--- vars/mavenReleaseStart.groovy
+++ vars/mavenReleaseStart.groovy
@@ -1,3 +1,5 @@
 def call(def releaseVersionParam, def developmentVersionParam) {
-     
+     withCredentials([usernamePassword(credentialsId: 'GIT_TEST', passwordVariable: 'AEM_PASSWORD', usernameVariable: 'AEM_USERNAME')]) {
+     bat "mvn clean jgitflow" :release-start -DreleaseVersion=1.0.0 -DdevelopmentVersion=1.0.0-SNAPSHOT"
+    }
 }
