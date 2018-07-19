f5ada6ba489be4c7df1cd75cfc4908bec0c63a10
hamza <hbenderouach@sqli.com>
2018-07-19 10:19:22 +0200

    Init project

--- vars/gitlabPurne.groovy
+++ vars/gitlabPurne.groovy
@@ -1,6 +1,6 @@
 def call(def urlGit) {
      withCredentials([usernamePassword(credentialsId: 'GIT_TEST', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-		        //bat "git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@${url}"	 
-		        //bat "git fetch --all --prune"
+		        bat "git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@${url}"	 
+		        bat "git fetch --all --prune"
 	 }
 }
--- vars/mavenReleaseStart.groovy
+++ vars/mavenReleaseStart.groovy
@@ -1,5 +1,3 @@
 def call(def releaseVersionParam, def developmentVersionParam) {
-     withCredentials([usernamePassword(credentialsId: 'GIT_TEST', passwordVariable: 'AEM_PASSWORD', usernameVariable: 'AEM_USERNAME')]) {
-     bat "mvn clean jgitflow" :release-start -Dusername='${AEM_USERNAME}' -Dpassword='${AEM_PASSWORD}' -DreleaseVersion=1.0.0 -DdevelopmentVersion=1.0.0-SNAPSHOT"
-    }
+     
 }
